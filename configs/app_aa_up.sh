555fd621ebcd4bcc479c61ae44530669d774eaf7
katafoxi <katafoxi@gmail.com>
2025-01-22 11:12:00 +0300

    rename

---
+++ configs/app_aa_up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+ deepstream-app -c /home/jet_user/wdir/smoke_detect/configs/y_app.yml
+
---
+++ configs/seg_aa_up.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ /home/jet_user/wdir/smoke_detect/src/deepstream-segmentation-custom  \
+ /home/jet_user/wdir/smoke_detect/configs/seg_ab_infer.yml \
+ /home/jet_user/wdir/smoke_detect/streams/OutputVideo1920p_yuv420p.mp4
+
+
