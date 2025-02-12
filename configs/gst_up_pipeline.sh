546fe8fdc42e9f9d793e9303232db23149a45a6e
katafoxi <katafoxi@gmail.com>
2025-02-12 08:15:00 +0300

    add commands to playing

--- configs/gst_up_pipeline.sh
+++ configs/gst_up_pipeline.sh
@@ -1,6 +1,6 @@
 gst-launch-1.0  \
 uridecodebin \
-    uri= file:///ssd/wdir/smoke_detect/streams/OutputVideo1920p_yuv420p.mp4 \
+    uri=file:///ssd/wdir/smoke_detect/streams/OutputVideo1920p_yuv420p.mp4 \
     name=dec \
     ! \
 m.sink_0 nvstreammux \
@@ -36,17 +36,64 @@ nvdsosd \
     process-mode=1 \
     qos=1 \
     ! \
-nvvideoconvert  \
-    ! \
-video/x-raw, format=NV12 \
-    ! \
-nv3dsink \
-    async=0 \
+nvvidconv ! video/x-raw,format=NV12 ! \
+x264enc  speed-preset=superfast  ! \
+rtph264pay  ! \
+udpsink  \
+    host=127.0.0.1 \
+    port=5000 
+    # sync=false \
+    # auto-multicast=0
+
+
+# h264parse ! \
+# mpegtsmux ! \
+# rtpmp2tpay ! \
+
+#######
+<<PLAY_COMMANDS
+udpsin + sdp_file 
+https://gist.github.com/esrever10/7d39fe2d4163c5b2d7006495c3c911bb?permalink_comment_id=3301904#gistcomment-3301904
+
+# test playing
+gst-launch-1.0 \
+udpsrc port=5000 \
+! application/x-rtp,media=video,encoding-name=H264 \
+! rtph264depay \
+! avdec_h264 \
+! autovideosink
+
+gst-launch-1.0 \
+udpsrc port=5000 \
+! application/x-rtp,media=video,encoding-name=H264 \
+! rtpmp2tpay \
+! avdec_h264 \
+! autovideosink
+
+gst-launch-1.0 \
+-v \
+udpsrc multicast-group=224.100.10.10 port=3001 \
+! application/x-rtp,media=video,encoding-name=MP2T,clock-rate=90000, payload=33 \
+! rtpjitterbuffer latency=300 
+! rtpmp2tdepay \
+! decodebin \
+! autovideosink
+
+PLAY_COMMANDS
+
 
     
-# x264enc \
-#     bitrate=2000000 \
-#     ! \
+
+# gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,format=MJPG,width=1280,height=720,framerate=30/1 ! nvv4l2decoder mjpeg=1 ! nvvidconv ! video/x-raw, format=NV12 ! x264enc speed-preset=superfast ! rtph264pay ! udpsink port=5000 host=127.0.0.1
 
 
-
+# https://forums.developer.nvidia.com/t/vlc-cant-play-video-stream-from-orin-agx/230761/7
+# out_pipeline_str << "appsrc ! video/x-raw, format=(string)BGR ! "
+# "videoconvert ! video/x-raw, format=(string)BGRx ! 
+# nvvidconv ! "
+# "video/x-raw(memory:NVMM), format=(string)NV12 ! 
+# nvv4l2h264enc insert-sps-pps=true insert-vui=true idrinterval=15 ! "
+# "h264parse ! 
+# mpegtsmux ! 
+# rtpmp2tpay !
+# udpsink host=HOST_IP port=8001 auto-multicast=0";
