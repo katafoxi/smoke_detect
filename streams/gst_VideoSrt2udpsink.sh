2bbe7f1ad04071094496324609d2885d300879fc
katafoxi <katafoxi@gmail.com>
2025-02-27 14:34:54 +0300

    mqueue-mux-udp

--- streams/gst_VideoSrt2udpsink.sh
+++ streams/gst_VideoSrt2udpsink.sh
@@ -1,23 +1,30 @@
+#!/bin/sh
+
+HOST=127.0.0.1
+PORT=5000
+
 # --gst-debug=*:1 --gst-debug=GST_CAPS:4 \
 gst-launch-1.0  -e \
-queue name=rtpqueue!\
+mqueue.src_0 ! queue2 !\
 rtpmux name=mux !\
-udpsink  host=127.0.0.1 port=5000 \
+udpsink  host=${HOST}  port=${PORT} \
+\
+mqueue.src_1 ! queue2 ! mux. \
 \
 filesrc location=OutputVideo1920p_yuv420p.mp4 !\
     decodebin !\
     nvvidconv !\
-    queue !\
-    x264enc  speed-preset=superfast ! \
-    queue !\
-    rtph264pay  pt=96 ! \
-    mux.sink_0 \
+    x264enc !\
+    queue2 !\
+    rtph264pay pt=96 !\
+    multiqueue name=mqueue \
 \
 filesrc location=OutputVideo1920p_yuv420p.srt !\
     subparse !\
-    queue !\
+    queue2 !\
     rtpgstpay pt=98 !\
-    mux.sink_1
+    mqueue. 
+
 
 # qtdemux name=video_demux video_demux.video_0  \
 #  application/x-rtp,payload=96,rate=90000
