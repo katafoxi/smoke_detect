fe020b8b256b1dc077b1811a4c071e99d937268c
katafoxi <katafoxi@gmail.com>
2025-04-22 10:54:24 +0300

    add path to gps_to_srt plugin

--- segm_app_up.sh
+++ segm_app_up.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+GPS_TO_SRT_PLUGIN_PATH="/wdir/gps_parse_plugin/builddir/plugin/"
+export GST_PLUGIN_SYSTEM_PATH_1_0="/lib/aarch64-linux-gnu/gstreamer-1.0:${GPS_TO_SRT_PLUGIN_PATH}"
+export GST_PLUGIN_PATH="/lib/aarch64-linux-gnu/gstreamer-1.0:${GPS_TO_SRT_PLUGIN_PATH}"
+
+echo "$GST_PLUGIN_SYSTEM_PATH_1_0"
+
+# gst-launch-1.0 \
+#   filesrc location=test.mp4 ! \
+#     decodebin name=dec \
+#     dec. ! queue ! nvvidconv ! textoverlay name=overlay ! autovideosink \
+#   mysrc ! overlay.
+
  /wdir/smoke_detect/src/segmsmoke_app \
  /wdir/smoke_detect/configs/segm_aa_infer_conf.txt \
  file:///wdir/smoke_detect/streams/OutputVideo1920p_yuv420p.mp4
