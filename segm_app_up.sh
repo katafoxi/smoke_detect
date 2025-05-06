7a5c716f4b053b17b7357171bd149edee0ccfb60
katafoxi <katafoxi@gmail.com>
2025-05-06 13:53:59 +0300

    fix deepstream include path

--- segm_app_up.sh
+++ segm_app_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GPS_TO_SRT_PLUGIN_PATH="/wdir/gps_parse_plugin/builddir/plugin/"
-DEEPSTREAM_PLUGIN_PATH="/opt/nvidia/deepstream/deepstream-6.0/lib/gst-plugins"
+DEEPSTREAM_PLUGIN_PATH="/opt/nvidia/deepstream/deepstream-7.0/lib/gst-plugins"
 export GST_PLUGIN_SYSTEM_PATH_1_0="/lib/aarch64-linux-gnu/gstreamer-1.0:${GPS_TO_SRT_PLUGIN_PATH}:${DEEPSTREAM_PLUGIN_PATH}"
 export GST_PLUGIN_PATH="/lib/aarch64-linux-gnu/gstreamer-1.0:${GPS_TO_SRT_PLUGIN_PATH}:${DEEPSTREAM_PLUGIN_PATH}"
 
